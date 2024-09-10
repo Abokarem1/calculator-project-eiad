076a66864bf390e8e63ef2619b53112bf57e2511
Abokarem1 <103247561+Abokarem1@users.noreply.github.com>
2024-09-10 14:29:05 +0200

    2m lsos

--- tests/test_API.py
+++ tests/test_API.py
@@ -6,15 +6,7 @@ from calculator_client.models.opertions import Opertions
 from calculator_client.models.result_response import ResultResponse
 
 class TestCalculatorAPI():
-    def test_calculator_add(self):
-        url = "http://localhost:5000/calculate"
-
-        payload = {
-            "operation": "add",
-            "operation1": 1,
-            "operation2": 2,
-        }
-        response = requests.post(url, json=payload)
+        
     def test_generated_code_test(self):
         client = Client("http://localhost:5000")
         response = calculate.sync(client = client, body = Calculation(Opertions.ADD, operand1=1, operand2=1))
