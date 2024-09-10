d3493dcabd94666017b0999b86c691effbb929c4
Abokarem1 <103247561+Abokarem1@users.noreply.github.com>
2024-09-10 12:55:27 +0200

    sho ma bdk

--- BE/calculator_helper.py
+++ BE/calculator_helper.py
@@ -1,10 +1,9 @@
-import logging
-import logger  # Anta att detta är en modul där du konfigurerar loggern
+import logger
 
-class CalculatorHelper:
+class CalculatorHelper():
     log_properties = {
         'custom_dimensions': {
-            'userId': 'MohamadEyad_Abouselo'
+            'userId': 'MohamadEyad-Abouselo'
         }
     }
 
@@ -18,23 +17,14 @@ class CalculatorHelper:
 
     def __init__(self):
         if not self._is_initialized:
-            # Setup logger
-            self.logger = logging.getLogger(__name__)
-            self.logger.setLevel(logging.DEBUG)
-            # You might need to set up handlers and formatters depending on your logger configuration
-            # Example:
-            # handler = logging.StreamHandler()
-            # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-            # handler.setFormatter(formatter)
-            # self.logger.addHandler(handler)
-            
             self._user_list = []
             self._current_user = None
-            admin = self.User('admin', 'test1234')
+            admin = self.User('admin','test1234')
             self._user_list.append(admin)
             self._is_initialized = True
+            self.logger = logger.get_logger(__name__)
 
-    class User:
+    class User():
         def __init__(self, username, password):
             self.username = username
             self.password = password
@@ -43,48 +33,44 @@ class CalculatorHelper:
             return f"User(username={self.username}, password={self.password})"
 
     def add(self, a, b):
-        self.logger.debug(f"add {a} + {b}", extra=self.log_properties)
-        return a + b
+        #self.logger.debug("test")
+        result =  a + b
+        self.logger.debug("Adding " + str(a)+ " to "+ str(b)+  "result" + str(result), extra=self.log_properties)
+        return result
 
     def subtract(self, a, b):
-        self.logger.debug(f"subtract {a} - {b}", extra=self.log_properties)
+        self.logger.debug("Subtracting " + str(a)+ " from "+ str(b), extra=self.log_properties)
         return a - b
 
     def multiply(self, a, b):
-        self.logger.debug(f"multiply {a} * {b}", extra=self.log_properties)
+        self.logger.debug("Multiplying " + str(a)+ " by "+ str(b), extra=self.log_properties)
         return a * b
 
-    def divide(self, a, b):
-        if b == 0:
-            self.logger.error("Division by zero is not allowed.", extra=self.log_properties)
-            raise ValueError("Cannot divide by zero")
-        self.logger.debug(f"divide {a} / {b}", extra=self.log_properties)
-        return a / b
+    def divide(self, a, b): 
+        self.logger.debug("Dividing " + str(a)+ " by "+ str(b) , extra=self.log_properties)
+        return a/b
 
     def register_user(self, username, password):
         for user in self._user_list:
-            if user.username == username:
-                self.logger.debug(f"Registreringsförsök misslyckades: Användarnamnet {username} är redan taget.", extra=self.log_properties)
+            if(user.username == username):
                 return None
-        
         user = self.User(username, password)
         self._user_list.append(user)
-        self.logger.debug(f"Ny användare registrerad: {username}.", extra=self.log_properties)
+        self.logger.debug("Registering user :{user}", extra=self.log_properties)
         return username
 
     def login(self, username, password):
         for user in self._user_list:
-            if user.username == username and user.password == password:
+            if(user.username == username and user.password == password):
                 self._current_user = user
-                self.logger.debug(f"username {username} har loggat in.", extra=self.log_properties)
+                self.logger.debug("The user :{user} is logging in", extra=self.log_properties)
                 return username
         return None
 
     def logout(self):
         user = self._current_user
-        if user:
-            self.logger.debug(f"Användaren {user.username} har loggat ut.", extra=self.log_properties)
         self._current_user = None
+        self.logger.debug("The user :{user} is logging out", extra=self.log_properties)
         return user
 
     def get_current_user(self):
